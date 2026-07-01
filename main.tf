52e3a6fe483412bb5bc5c179491e2cd9a743e3a3
CrisLB27 <cristoballealbelmar@gmail.com>
2026-07-01 19:13:52 -0400

    chore: release version 0.4.0

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 module "redes" {
   source = "git::https://github.com/crlealb/terraform-aws-vpc-auy1105-cr.git?ref=1.0.0"
 
