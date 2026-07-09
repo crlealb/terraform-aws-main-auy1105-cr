e858bc026bee9593be57ad7fb7583daa31b96c0e
CrisLB27 <cristoballealbelmar@gmail.com>
2026-07-09 05:04:35 -0400

    chore: release version 1.0.0 estable

--- import.tf
+++ import.tf
@@ -1,47 +1,38 @@
-# Archivo de importación para recursos ya creados en AWS.
-# Sustituye los IDs de ejemplo por los valores reales de tus recursos.
+# Archivo de importación preparado para futuros entornos.
+# Los bloques quedan comentados para que terraform validate no falle
+# mientras el recurso no está definido en la configuración actual.
 
-import {
-  to = module.computo.aws_instance.web
-  id = "i-1234567890abcdef0"
-}
+# import {
+#   to = module.redes.aws_vpc.main
+#   id = "vpc-xxxxxxxxxxxxxxxx"
+# }
 
-import {
-  to = module.redes.aws_vpc.main
-  id = "vpc-1234567890abcdef0"
-}
+# import {
+#   to = module.redes.aws_subnet.public_1
+#   id = "subnet-xxxxxxxxxxxxxxxx"
+# }
 
-import {
-  to = module.redes.aws_subnet.public[0]
-  id = "subnet-1234567890abcdef0"
-}
+# import {
+#   to = module.redes.aws_subnet.public_2
+#   id = "subnet-xxxxxxxxxxxxxxxx"
+# }
 
-import {
-  to = module.redes.aws_subnet.public[1]
-  id = "subnet-1234567890abcdef0"
-}
+# import {
+#   to = module.redes.aws_subnet.private
+#   id = "subnet-xxxxxxxxxxxxxxxx"
+# }
 
-import {
-  to = module.redes.aws_subnet.private[0]
-  id = "subnet-1234567890abcdef0"
-}
+# import {
+#   to = module.computo.aws_instance.web
+#   id = "i-xxxxxxxxxxxxxxxx"
+# }
 
-import {
-  to = module.redes.aws_security_group.web
-  id = "sg-1234567890abcdef0"
-}
+# import {
+#   to = module.loadbalancer.aws_lb.main
+#   id = "arn:aws:elasticloadbalancing:us-east-1:123456789012:loadbalancer/app/nombre-alb/xxxxxxxxxxxxxxxx"
+# }
 
-import {
-  to = module.redes.aws_security_group.alb
-  id = "sg-1234567890abcdef0"
-}
-
-import {
-  to = module.loadbalancer.aws_lb.this
-  id = "arn:aws:elasticloadbalancing:us-east-1:123456789012:loadbalancer/app/mi-alb/1234567890abcdef"
-}
-
-import {
-  to = module.storage.aws_s3_bucket.storage
-  id = "eva2-storage-bucket-cr-dev"
-}
+# import {
+#   to = module.storage.aws_s3_bucket.storage
+#   id = "mi-bucket-s3"
+# }
